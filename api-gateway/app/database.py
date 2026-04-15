4ad66b04f3d2676e6eab697ffbeeef26b699a421
Uday Manikanta <udaymanikanta90@gmail.com>
2026-04-16 00:41:40 +0530

    Fixed DB connection for Render

--- api-gateway/app/database.py
+++ api-gateway/app/database.py
@@ -1,21 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-import time
+import os
 
-DATABASE_URL = "postgresql://marine:marine123@postgres:5432/marine_db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-# Retry connection
-for i in range(10):
-    try:
-        engine = create_engine(DATABASE_URL)
-        engine.connect()
-        print("Database connected successfully")
-        break
-    except Exception:
-        print("Database not ready, retrying...")
-        time.sleep(3)
+engine = create_engine(DATABASE_URL)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
 Base = declarative_base()
 
 def get_db():
